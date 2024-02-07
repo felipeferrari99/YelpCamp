10bd226b63bf3a6f266006c2fef7ee031f78928c
felipeferrari99 <felipe.ferrari@pedbot.com.br>
2024-02-07 17:10:23 -0300

    Adição de Reviews

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ const app = express();
 const path = require("path");
 const port = 3000;
 const ejsMate = require("ejs-mate");
-const {campgroundSchema} = require('./schemas.js');
+const {campgroundSchema, reviewSchema} = require('./schemas.js');
 const catchAsync = require('./utils/catchAsync.js');
 const ExpressError = require('./utils/ExpressError.js');
 const methodOverride = require("method-override");
-var connection = require('./database/db.js');
+var con = require('./database/db.js');
 
 app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
@@ -25,12 +25,22 @@ const validateCampground = (req, res, next) => {
   }
 }
 
+const validateReview = (req, res, next) => {
+  const {error} = reviewSchema.validate(req.body);
+  if(error) {
+    const msg = error.details.map(el => el.message).join(',');
+    throw new ExpressError(msg, 400);
+  } else {
+    next();
+  }
+}
+
 app.get('/', (req, res) => {
   res.render('home');
 });
 
 app.get('/campgrounds', catchAsync(async (req, res) => {
-  connection.query('SELECT * FROM campgrounds', function (err, campgrounds) {
+  con.query('SELECT * FROM campgrounds', function (err, campgrounds) {
     res.render('campgrounds/index', { campgrounds });
   });
 }));
@@ -41,22 +51,24 @@ app.get('/campgrounds/new', (req, res) => {
   
 app.post('/campgrounds', validateCampground, catchAsync(async (req, res) => {
   const { title, image, price, description, location } = req.body;
-  const [campground] = await connection.promise().query('INSERT INTO campgrounds (title, image, price, description, location) VALUES (?, ?, ?, ?, ?)', [title, image, price, description, location]);
+  const [campground] = await con.promise().query('INSERT INTO campgrounds (title, image, price, description, location) VALUES (?, ?, ?, ?, ?)', [title, image, price, description, location]);
   const newCampgroundId = campground.insertId;
   res.redirect(`/campgrounds/${newCampgroundId}`);
 }));
 
 app.get('/campgrounds/:id', catchAsync(async (req, res) => {
   const { id } = req.params;
-  connection.query('SELECT * FROM campgrounds WHERE id = ?', [id], function (err, results) {
+  con.query('SELECT * FROM campgrounds WHERE id = ?', [id], function (err, results) {
     const campground = results[0];
-    res.render('campgrounds/show', { campground });
-  });
+    con.query('SELECT id, body, rating FROM reviews WHERE campground_id = ?', id, (err, reviewResults) => {
+      const reviews = reviewResults;
+      res.render('campgrounds/show', { campground, reviews });
+  })});
 }));
 
 app.get('/campgrounds/:id/edit', catchAsync(async (req, res) => {
   const { id } = req.params;
-  connection.query('SELECT * FROM campgrounds WHERE id = ?', [id], function (err, results) {
+  con.query('SELECT * FROM campgrounds WHERE id = ?', [id], function (err, results) {
     const campground = results[0];
     res.render('campgrounds/edit', { campground });
   });
@@ -65,15 +77,31 @@ app.get('/campgrounds/:id/edit', catchAsync(async (req, res) => {
 app.put('/campgrounds/:id', validateCampground, catchAsync(async (req, res) => {
   const { title, image, price, description, location } = req.body;
   const { id } = req.params;
-  const [results] = await connection.promise().query('UPDATE campgrounds SET title = ?, image = ?, price = ?, description = ?, location = ? WHERE id = ?', [title, image, price, description, location, id]);
+  await con.promise().query('UPDATE campgrounds SET title = ?, image = ?, price = ?, description = ?, location = ? WHERE id = ?', [title, image, price, description, location, id]);
   res.redirect(`/campgrounds/${id}`);
 }));
   
 app.delete('/campgrounds/:id', catchAsync(async (req, res) => {
   const { id } = req.params;
-  const [results] = await connection.promise().query('DELETE FROM campgrounds WHERE id = ?', [id]);
+  await con.promise().query('DELETE FROM reviews WHERE campground_id = ?', [id]);
+  await con.promise().query('DELETE FROM campgrounds WHERE id = ?', [id]);
   res.redirect('/campgrounds');
 }));
+
+
+app.post('/campgrounds/:id/reviews', validateReview, catchAsync(async (req, res) => {
+  const campgroundId = req.params.id;
+  const { body, rating } = req.body;
+  await con.promise().query('INSERT INTO reviews (body, rating, campground_id) VALUES (?, ?, ?)', [body, rating, campgroundId]);
+  res.redirect(`/campgrounds/${campgroundId}`);
+}))
+
+app.delete('/campgrounds/:id/reviews/:reviewId', catchAsync(async (req, res) => {
+  const campgroundId = req.params.id;
+  const { reviewId } = req.params;
+  await con.promise().query('DELETE FROM reviews WHERE id = ?', [reviewId]);
+  res.redirect(`/campgrounds/${campgroundId}`);
+}))
 
 app.all('*', (req, res, next) => {
   next(new ExpressError('Page Not Found', 404));
--- database/db.js
+++ database/db.js
@@ -1,37 +1,71 @@
 const mysql = require('mysql2');
-const tableName = 'campgrounds';
+const campgroundsTableName = 'campgrounds';
+const reviewsTableName = 'reviews';
 
-const connection = mysql.createConnection({
+const con = mysql.createConnection({
   host: 'mysql_db',
   user: 'root',
   password: '1234',
   database: 'yelpCamp',
 });
   
-connection.connect((err) => {
+con.connect((err) => {
   if (err) {
     console.error('Error connecting to MySQL:', err);
   } else {
     console.log('Connected to MySQL database');
-    connection.query(`SHOW TABLES LIKE '${tableName}'`, (err, rows) => {
+    con.query(`SHOW TABLES LIKE '${campgroundsTableName}'`, (err, rows) => {
       if (rows.length === 0) {
-        const createTableSql = `
-        CREATE TABLE ${tableName} (
-          id INT AUTO_INCREMENT PRIMARY KEY,
-          title VARCHAR(255) NOT NULL,
-          image VARCHAR(255),
-          price FLOAT NOT NULL,
-          description VARCHAR(255),
-          location VARCHAR(255) NOT NULL
-        )`;
-      
-        connection.query(createTableSql, (err, result) => {
-          if (err) throw err;
-          console.log('Table created');
+        const createCampgroundsTableSql = `
+          CREATE TABLE ${campgroundsTableName} (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            title VARCHAR(255) NOT NULL,
+            image VARCHAR(255),
+            price FLOAT NOT NULL,
+            description VARCHAR(255),
+            location VARCHAR(255) NOT NULL
+          )`;
+        
+        con.query(createCampgroundsTableSql, (err, result) => {
+          if (err) {
+            console.error('Error creating campgrounds table:', err);
+            return;
+          }
+          console.log('Campgrounds table created');
+          checkReviewsTable();
         });
+      } else {
+        checkReviewsTable();
       }
     });
   }
-})
+});
 
-module.exports = connection
+function checkReviewsTable() {
+  con.query(`SHOW TABLES LIKE '${reviewsTableName}'`, (err, rows) => {
+    if (rows.length === 0) {
+      createReviewsTable();
+    }
+  });
+}
+
+function createReviewsTable() {
+  const createReviewsTableSql = `
+    CREATE TABLE ${reviewsTableName} (
+      id INT AUTO_INCREMENT PRIMARY KEY,
+      body TEXT NOT NULL,
+      rating INT NOT NULL,
+      campground_id INT,
+      FOREIGN KEY (campground_id) REFERENCES ${campgroundsTableName}(id)
+    )`;
+  
+  con.query(createReviewsTableSql, (err, result) => {
+    if (err) {
+      console.error('Error creating reviews table:', err);
+      return;
+    }
+    console.log('Reviews table created');
+  });
+}
+
+module.exports = con;
